6a8c27dcc0d7ad91132eae83bce7a52c42707ca6
AqUpd <aquupd@ya.ru>
2022-06-26 15:48:39 +0300

    update version number

--- src/main/java/com/aqupd/chatdtimer/ChatDTimer.java
+++ src/main/java/com/aqupd/chatdtimer/ChatDTimer.java
@@ -11,7 +11,7 @@ public class ChatDTimer {
 
 	public static final String MOD_ID = "chatdtimer";
 	public static final String MOD_NAME = "Unfocused Chat Delay";
-	public static final String VERSION = "1.0";
+	public static final String VERSION = "1.0.1";
 	public static final Logger LOGGER = LogManager.getLogger();
 	public static String logprefix = "[AqUpd's " + MOD_NAME + "] ";
 
