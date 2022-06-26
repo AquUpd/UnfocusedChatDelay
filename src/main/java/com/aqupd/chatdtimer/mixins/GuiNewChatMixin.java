6c339cbfba795dcf91c3f27431bf18a76844f31b
AqUpd <aquupd@ya.ru>
2022-06-26 18:13:19 +0300

    change priority to 10001

--- src/main/java/com/aqupd/chatdtimer/mixins/GuiNewChatMixin.java
+++ src/main/java/com/aqupd/chatdtimer/mixins/GuiNewChatMixin.java
@@ -7,7 +7,7 @@ import net.minecraft.client.gui.GuiNewChat;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.*;
 
-@Mixin(GuiNewChat.class)
+@Mixin(value = GuiNewChat.class, priority = 10001)
 public class GuiNewChatMixin extends Gui {
   @ModifyArg(
     method = "printChatMessageWithOptionalDeletion(Lnet/minecraft/util/IChatComponent;I)V",
